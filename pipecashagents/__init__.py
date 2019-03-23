b69ac6b4d3a1a020044f62776b02c08e80a2a54f
Bitcoin Sofia <alexander.n.dinkov@gmail.com>
2019-03-23 15:51:28 +0200

    remove non-agents from __init__

--- pipecashagents/__init__.py
+++ pipecashagents/__init__.py
@@ -7,8 +7,6 @@ __email__ = 'alexander.n.dinkov@gmail.com'
 __version__ = '0.1.1'
 
 from pipecashagents.Email.Email import *
-from pipecashagents.Exchanges.Binance import *
 from pipecashagents.WalletAgents.WalletAgents import *
 
-from pipecashagents.ExampleAgent import *
 
