bba479cee7a20fa545e1e720f91ed8f5f7b21902
Bitcoin Sofia <alexander.n.dinkov@gmail.com>
2019-03-23 15:54:48 +0200

    test - can import wallets

--- tests/test_pipecashagents.py
+++ tests/test_pipecashagents.py
@@ -3,16 +3,7 @@
 
 """Tests for `pipecashagents` package."""
 
-
 import unittest
-
-from pipecashagents import Binance_Buy
-from pipecashagents import Binance_GetBalance
-from pipecashagents import Binance_Withdraw
-from pipecashagents import Email_Send
-from pipecashagents import WalletAgent_GetReceiveAddress
-from pipecashagents import WalletAgent_OnBalanceChange
-
 
 class TestPipecashAgents(unittest.TestCase):
     """Tests for `pipecashagents` package."""
@@ -23,5 +14,7 @@ class TestPipecashAgents(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+    def test_canImportWallets(self):
+        from pipecashagents import Email_Send
+        from pipecashagents import WalletAgent_GetReceiveAddress
+        from pipecashagents import WalletAgent_OnBalanceChange
