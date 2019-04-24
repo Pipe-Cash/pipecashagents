b953fe3a924a62d0fc7c30a87abae111ab4d60dd
Bitcoin Sofia <alexander.n.dinkov@gmail.com>
2019-04-25 01:02:44 +0300

    request import as agent dependancy

--- pipecashagents/HandCash/GetHandCashAddress.py
+++ pipecashagents/HandCash/GetHandCashAddress.py
@@ -1,13 +1,15 @@
 import json
-import requests
 
 class GetHandCashAddress:
 
     def start(self, log):
         self.log = log
+        import requests
+        self.requests = requests
+        
 
     def __init__(self):
-
+        
         self.description = '''
         Gets a bitcoin public address corresponding to the given HandCash handle
 
@@ -28,6 +30,9 @@ class GetHandCashAddress:
             "publicKey": "024343375e863f2c35fec81295665c9da5501df6f5816b5ef745e332dc5f188efb"
         }
 
+    def check_dependencies_missing(self):
+        import requests
+
     def validate_options(self):
         assert "handle" in self.options,"'handle' not in options"
 
@@ -37,7 +42,7 @@ class GetHandCashAddress:
         handle = handle[1:] if handle.startswith('$') else handle
         assert len(handle) > 0, "no handcash handle given"
         
-        r = requests.get("http://api.handcash.io/api/receivingAddress/" + handle)
+        r = self.requests.get("http://api.handcash.io/api/receivingAddress/" + handle)
         response = json.loads(r.text)
 
         create_event({
