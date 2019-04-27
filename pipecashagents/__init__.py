79f3c9f8889b0c28dc1f5c67f6ed38dd8cbb02bc
Bitcoin Sofia <alexander.n.dinkov@gmail.com>
2019-04-27 21:04:20 +0300

    version 0.1.2.5

--- pipecashagents/__init__.py
+++ pipecashagents/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Aleksandar Nikolaev Dinkov"""
 __email__ = 'alexander.n.dinkov@gmail.com'
-__version__ = '0.1.2.4'
+__version__ = '0.1.2.5'
 
 from pipecashagents.Email.Email import *
 from pipecashagents.WalletAgents.WalletAgents import *
